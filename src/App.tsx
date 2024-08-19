16cc6b2b4c1257d5dbba045210945069cf065210
abhinav <contact.abhinavanshul@gmail.com>
2024-08-19 18:13:14 +0530

    fix : redirection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import "./App.css"
-import { Route, Switch } from "wouter"
+import { Route, Switch, useLocation } from "wouter"
 import { Dashboard, Home, Login, Signup } from "./components"
+import { useEffect } from "react"
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [, setLocation] = useLocation()
+
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      setLocation("/dashboard")
+    }
+  }, [setLocation])
 
   return (
     <>
       <Switch>
+        {/* // a homepage component if needed for landing, right now I'm redirecting the user to dashboard */}
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
